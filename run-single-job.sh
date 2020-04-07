a9f5eaa773ac3419bf3fca5d4341ae3c9ebe3b5e
seamus100 <rawhox@gmail.com>
2020-04-06 21:10:35 -0600

    blah

--- run-single-job.sh
+++ run-single-job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --time=05:00:00
+#SBATCH --time=01:00:00
 #SBATCH --output=run-single-out-%j
 #SBATCH --error=run-single-error-%j 
 #SBATCH --mail-type=ALL 
-#SBATCH --mail-user=yourname@yourmail.edu
+#SBATCH --mail-user=scalden@colostate.edu
 
 
 ./map-N-files-from-K.sh 128 1
